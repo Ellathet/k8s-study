b373108fcbeb74b020a4c6daa3e4bf2d17d1e625
Mikha Thet <social.amais@gmail.com>
2024-01-22 22:01:16 -0300

    chore: variables in k8s

--- server.go
+++ server.go
@@ -1,12 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/foods", ConfigMap)
 	http.ListenAndServe(":80", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello!"))
+
+	name := os.Getenv("NAME")
+	fmt.Fprint(w, "Hello, I'm %s.", name)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+
+	data, err := ioutil.ReadFile("/go/foods/foods.txt")
+	if err != nil {
+		log.Fatalf("error reading file", err)
+	}
+
+	fmt.Fprint(w, "My foods are: %s.", string(data))
 }
